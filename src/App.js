382571dbc0842f31e1370342cfbe2599db56d089
ahmetsarica <ahmetsarica.fe@gmail.com>
2020-11-13 17:29:44 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 import './App.css';
+import Image from './image.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    state = {
+        bottomImagePosition: -300,
+        topImagePosition:0,
+        dropAreaHeight: 300,
+        topImageContainer: 300,
+        bottomImageContainer: 300,
+    };
+    handleStop = (e, ui) => {
+        const { y } = ui;
+        if (y < 150) {
+            this.setState({
+                topImageContainer: y,
+                bottomImageContainer: 150 + (150 - y),
+                bottomImagePosition: 40 - y,
+            })
+        } else {
+            this.setState({
+                topImageContainer: y,
+                bottomImageContainer: 300-y,
+                bottomImagePosition: 40 - y,
+            })
+        }
+    };
+
+    render(){
+        const { bottomImagePosition, dropAreaHeight, topImagePosition, topImageContainer, bottomImageContainer } = this.state;
+        debugger
+        return (
+            <div className="container">
+                <div style={{height: dropAreaHeight + 'px'}} className="drop-area">
+                    <div style={{height: topImageContainer+'px'}} className="top-image-container">
+                        <img style={{top: topImagePosition + 'px'}} src={Image} className="top-image" />
+                    </div>
+                    <div style={{height: bottomImageContainer+'px'}} className="bottom-image-container">
+                        <img style={{top: bottomImagePosition + 'px'}} src={Image} className="bottom-image" />
+                    </div>
+                </div>
+                <Draggable
+                    defaultPosition={{x: 0, y: 0}}
+                    position={null}
+                    scale={1}
+                    onStop={this.handleStop}>
+                    <div className="drag-item">
+                        <div>Drag from here</div>
+                    </div>
+                </Draggable>
+            </div>
+        );
+    }
 }
 
 export default App;
